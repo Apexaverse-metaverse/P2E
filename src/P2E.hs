574f1d7cd6d03bd3903ff6b849bf572a7b5e0124
Walker <walker@praiseweb.com.br>
2022-04-18 08:07:51 -0300

    feat: add minting with permissive policy

---
+++ src/P2E.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveAnyClass      #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE NumericUnderscores  #-}
+
+
+module P2E where
+
+import           Control.Monad          hiding (fmap)
+import           Data.Aeson             (ToJSON, FromJSON)
+import           Data.Text              (Text)
+import           Data.Void              (Void)
+import           GHC.Generics           (Generic)
+import           Plutus.Contract        as Contract
+import           Plutus.Trace.Emulator  as Emulator
+import qualified PlutusTx
+import           PlutusTx.Prelude       hiding (Semigroup(..), unless)
+import           Ledger                 hiding (mint, singleton)
+import           Ledger.Constraints     as Constraints
+import qualified Ledger.Typed.Scripts   as Scripts
+import           Ledger.Value           as Value
+import           Playground.Contract    (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
+import           Playground.TH          (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types       (KnownCurrency (..))
+import           Prelude                (IO, Show (..), String)
+import           Text.Printf            (printf)
+import           Wallet.Emulator.Wallet
+
+tokenName :: TokenName
+tokenName = "APEXAVERSE"
+
+tokenSupply :: Integer
+tokenSupply = 10_000_000_000
+
+{-# INLINABLE mkPolicy #-}
+mkPolicy :: () -> ScriptContext -> Bool
+mkPolicy () _ = True
+
+policy :: Scripts.MintingPolicy
+policy = mkMintingPolicyScript $$(PlutusTx.compile [|| Scripts.wrapMintingPolicy mkPolicy ||])
+
+curSymbol :: CurrencySymbol
+curSymbol = scriptCurrencySymbol policy
+
+type FreeSchema = Endpoint "mint" MintParams
+
+mint :: Contract w FreeSchema Text ()
+mint = do
+    let val     = Value.singleton curSymbol tokenName tokenSupply
+        lookups = Constraints.mintingPolicy policy
+        tx      = Constraints.mustMintValue val
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    Contract.logInfo @String $ printf "forged %s" (show val)
+
+endpoints :: Contract () FreeSchema Text ()
+endpoints = mint' >> endpoints
+  where
+    mint' = awaitPromise $ endpoint @"mint" mint
+
+mkSchemaDefinitions ''FreeSchema
+
+mkKnownCurrencies []
+
+main :: IO ()
+main = runEmulatorTraceIO $ do
+    h1 <- activateContractWallet (knownWallet 1) endpoints
+    callEndpoint @"mint" h1
+    void $ Emulator.waitNSlots 1
