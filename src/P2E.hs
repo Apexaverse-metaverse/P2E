6017c92a2dbfbeae262d06e2a42fd1bed8abe207
Walker <walker@praiseweb.com.br>
2022-04-18 08:54:50 -0300

    feat: add write file policy function

--- src/P2E.hs
+++ src/P2E.hs
@@ -11,11 +11,14 @@
 {-# LANGUAGE TypeOperators       #-}
 {-# LANGUAGE NumericUnderscores  #-}
 
-module P2E (mint, policy, policyCode, curSymbol, main) where
+module P2E (mint, policy, policyCode, curSymbol, simulate, writePolicyFile) where
 
 import           Control.Monad          hiding (fmap)
+import           Control.Exception      (throwIO)
 import           Data.Text              (Text)
 import           Data.Void              (Void)
+import qualified Data.ByteString.Short  as SBS
+import qualified Data.ByteString.Lazy   as LBS
 import           Plutus.Contract        as Contract
 import           Plutus.Trace.Emulator  as Emulator
 import qualified PlutusTx
@@ -24,9 +27,12 @@ import           Ledger                 hiding (mint, singleton)
 import           Ledger.Constraints     as Constraints
 import qualified Ledger.Typed.Scripts   as Scripts
 import qualified Ledger.Value           as Value
-import           Prelude                (IO, Show (..), String)
+import           Prelude                (IO, Show (..), String,  userError, FilePath)
 import           Text.Printf            (printf)
 import           Wallet.Emulator.Wallet
+import           Cardano.Api            (PlutusScriptV1, FileError, writeFileTextEnvelope)
+import           Cardano.Api.Shelley    (PlutusScript (..))
+import           Codec.Serialise        (serialise)
 
 tokenName :: TokenName
 tokenName = "APEXAVERSE"
@@ -56,7 +62,18 @@ mint = do
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
     Contract.logInfo @String $ printf "forged %s" (show val)
 
-main :: IO ()
-main = runEmulatorTraceIO $ do
+writeMintingPolicy :: FilePath -> Scripts.MintingPolicy -> IO (Either (FileError ()) ())
+writeMintingPolicy file = writeFileTextEnvelope @(PlutusScript PlutusScriptV1) file Nothing . PlutusScriptSerialised . SBS.toShort . LBS.toStrict . serialise . getMintingPolicy
+
+writePolicyFile :: IO ()
+writePolicyFile = do
+    let file  = "policy.plutus"
+    e <- writeMintingPolicy file policy
+    case e of
+      Left err -> throwIO $ userError $ show err
+      Right () -> return ()
+
+simulate :: IO ()
+simulate = runEmulatorTraceIO $ do
     _ <- activateContractWallet (knownWallet 1) mint
     void $ Emulator.waitNSlots 1
